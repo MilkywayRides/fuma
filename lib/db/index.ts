15518606b6f64d7e0448500ab7578ece71c4c819
MilkywayRides <milkywayrides@gmail.com>
2025-10-22 16:00:11 +0530

    bug fix

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,10 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 
+if (typeof window !== 'undefined') {
+  throw new Error('Database should not be imported on client side');
+}
+
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
